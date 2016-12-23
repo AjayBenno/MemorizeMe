405deee14ea1edbe25b2c8dd289a1fd72d0d2a78
Ajay Benno <abenno@andrew.cmu.edu>
2016-12-23 00:56:12 -0500

    Working website enviroment that pushes data to server with random word selection

--- server/app.py
+++ server/app.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 from flask import Flask,render_template,request
 from firebase import firebase
-import random
+import random,json
 
-firebase = firebase.FirebaseApplication('https://memorize-me-ab784.firebaseio.com/')
+firebase = firebase.FirebaseApplication('https://memorize-me-ab784.firebaseio.com/',)
 wordfile = open('words.txt')
 words=[]
 app = Flask(__name__)
@@ -15,7 +15,17 @@ def getRandomWord():
 
     
 def getResultfromFirebase(text):
+    currentUsedWords = firebase.get('/text',None)
+    if(currentUsedWords != None and len(currentUsedWords) ==850):
+        firebase.delete('/text')
     word = getRandomWord()
+    while(currentUsedWords != None and word in currentUsedWords):
+        word = getRandomWord()
+    data = {word:text}
+    sent = json.dumps(data)
+    firebase.post('/text',sent)
+    return word
+
 
 
 @app.route('/')
@@ -26,7 +36,7 @@ def hello_world():
 def test():
     text = request.form.get('textarea1','a')
     key = getResultfromFirebase(text)
-    return text
+    return key
 
 
 if __name__ == '__main__':
