f92b554bcd5b8ad622d23563f666017140e8ee8d
Ajay Benno <abenno@andrew.cmu.edu>
2016-12-23 00:21:09 -0500

    Working Post, working on firebase integration

--- server/app.py
+++ server/app.py
@@ -1,10 +1,34 @@
 #!/usr/bin/env python
-from flask import Flask
+from flask import Flask,render_template,request
+from firebase import firebase
+import random
+
+firebase = firebase.FirebaseApplication('https://memorize-me-ab784.firebaseio.com/')
+wordfile = open('words.txt')
+words=[]
 app = Flask(__name__)
+def generateWords():
+    for word in wordfile:
+        words.append(word)
+def getRandomWord():
+    return random.choice(words)
+
+    
+def getResultfromFirebase(text):
+    word = getRandomWord()
+
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World'
+    return render_template('index.html',name="a")
+
+@app.route('/storeTextandGetName', methods=['POST'])
+def test():
+    text = request.form.get('textarea1','a')
+    key = getResultfromFirebase(text)
+    return text
+
 
 if __name__ == '__main__':
-    app.run()
+    generateWords()
+    app.run(debug=True)
